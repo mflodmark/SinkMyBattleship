3ec0bb07901e9ab641ff8379cd418ac18177cd8e
Markus Flodmark <markus.flodmark@gmail.com>
2018-12-06 10:51:29 +0100

    init

---
+++ SinkMyBattleshipWPF/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using SinkMyBattleshipWPF.ViewModels;
+using System.Windows;
+
+namespace SinkMyBattleshipWPF
+{
+    public class Bootstrapper: BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
